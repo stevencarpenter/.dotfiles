3fd4b65f7dd4f967d157475eee28e256b23c86a6
Steven Carpenter <steven@sjcarpenter.com>
2024-06-05 22:53:59 -0600

    Cleanup. NVIM will be wip while I play with it more.

--- home/.config/nvim/lua/plugins/telescope.lua
+++ home/.config/nvim/lua/plugins/telescope.lua
@@ -1,15 +1,30 @@
-return {{
-    -- Telescope
-    -- Find, Filter, Preview, Pick. All lua, all the time.
+return {
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+  },
+  {
     "nvim-telescope/telescope.nvim",
-    dependencies = {"nvim-lua/plenary.nvim", {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make"
-    }},
-    config = function(_)
-        require("telescope").setup()
-        -- To get fzf loaded and working with telescope, you need to call
-        -- load_extension, somewhere after setup function:
-        require("telescope").load_extension("fzf")
-    end
-}}
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+        },
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+
+      require("telescope").load_extension("ui-select")
+    end,
+  },
+}
