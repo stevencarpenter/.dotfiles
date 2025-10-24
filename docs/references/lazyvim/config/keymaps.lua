cfdb8be85b1adec9c5d632924bc8e9776b96b82f
Steven Carpenter <telelax@gmail.com>
2025-10-24 01:11:39 -0600

    Mcp configurations (#23)
    Fall cleaning

--- docs/references/lazyvim/config/keymaps.lua
+++
@@ -1,5 +0,0 @@
--- NOTE: This snapshot of LazyVim's keymaps was removed from the docs to keep the repository concise.
--- See docs/neovim-hotkeys.md for the consolidated keymap reference of this configuration.
--- Upstream LazyVim source is available at: https://github.com/LazyVim/LazyVim
-
-return {}
